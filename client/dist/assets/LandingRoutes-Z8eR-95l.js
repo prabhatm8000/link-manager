c4d91514c964e8bddd29f8674167234d057a312a
Prabhat Mishra <workforprabhat1254@gmail.com>
2025-05-05 01:13:59 +0530

    Ui build

--- client/dist/assets/LandingRoutes-Z8eR-95l.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-CVsHVHt9.js";import{N as t,R as e,j as a}from"./AppRouter-CpJfo_l_.js";const r=()=>s.jsx("div",{className:"",children:s.jsx(t,{})}),i=()=>s.jsx(e,{children:s.jsx(a,{path:"*",element:s.jsx(r,{})})});export{i as default};
