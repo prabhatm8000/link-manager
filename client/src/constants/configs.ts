b689116fecc1525cd0be64873bbeb227530d4b18
Prabhat Mishra <workforprabhat1254@gmail.com>
2025-05-05 01:07:47 +0530

    env setup in client

--- client/src/constants/configs.ts
+++ client/src/constants/configs.ts
@@ -1,6 +1,5 @@
 const configs = {
-    mode: "dev", 
-    // mode: "prod", 
+    mode: import.meta.env.VITE_MODE || "dev", 
     devBaseUrl: "http://localhost:1905/api/v1",
     serverBaseUrl: "/api/v1",
     googleClientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
