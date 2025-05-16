696506dc5c64f7310373c2f55d874093ca8c7c55
Prabhat Mishra <workforprabhat1254@gmail.com>
2025-05-16 23:06:26 +0530

    client build

---
+++ server/clientBuild/assets/LandingRoutes-CyBRzws8.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-BxgpHKSm.js";import{R as t,j as e,k as o}from"./AppRouter-BomXxFqA.js";const r=()=>s.jsx(t,{children:s.jsx(e,{path:"*",element:s.jsx(o,{})})});export{r as default};
